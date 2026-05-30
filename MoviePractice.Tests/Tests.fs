572f354bb4ebffa4416bb183dcc89a00fa1026c0
josiaschweizer <josia.schweizer@gmail.com>
2026-05-30 10:37:22 +0200

    created first test

--- MoviePractice.Tests/Tests.fs
+++ MoviePractice.Tests/Tests.fs
@@ -1,8 +1,36 @@
-﻿module Tests
+﻿namespace MoviePractice.Tests
 
-open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
 open Xunit
+open MoviePractice.Library
+open MoviePractice.Library.MovieService.MovieService
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+[<TestClass>]
+module TestMovieService() =
+
+    let movies: Movie list =
+        [ { Title = "Titel"
+            Year = 2018
+            Rating = 7.1
+            Duration = 96
+            Genres = [ "Action"; "Sci-Fi" ]
+            IsStreaming = true }
+          { Title = "Neu"
+            Year = 2025
+            Rating = 9.1
+            Duration = 124
+            Genres = [ "Action" ]
+            IsStreaming = false } ]
+
+    [<TestMethod>]
+    let testGetTitles_1 () =
+        let result = getTitles movies
+
+        Assert.Equal<string list>([ "Titel"; "Neu" ], result)
+
+    [<TestMethod>]
+    let testGetTitles_2 () =
+        let testMovies: Movie list = []
+        let result = getTitles testMovies
+
+        Assert.Equal<string list>([], result)
